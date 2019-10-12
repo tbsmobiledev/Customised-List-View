19eaace0fe8b383878657bba019aff90d71129cb
Sazzad Iproliya <sazzad.iproliya@gmail.com>
2019-10-12 14:17:34 +0530

    Initial commit of customised list view with image preview.

---
+++ Customised List View/Customised List View/bridging_header.h
@@ -0,0 +1,13 @@
+//
+//  Registration_bridging_header.h
+//  Registration_demo
+//
+//  Created by TBS17 on 16/09/19.
+//  Copyright © 2019 Sazzadhusen Iproliya. All rights reserved.
+//
+
+#ifndef Registration_bridging_header_h
+#define Registration_bridging_header_h
+
+
+#endif /* Registration_bridging_header_h */
